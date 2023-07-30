3b7f4fae538a8f34e528af51304888174a3800c9
DanielL24 <140912293+sirpromptelote@users.noreply.github.com>
2023-07-30 22:34:02 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Tech Quests and Heartfelt Bonds - Technical Documentation  </span>,
+  logo: <span>Tech Quests and Heartfelt Bonds - WIKI  </span>,
   footer: {
     text: (
       <span>
